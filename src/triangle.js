721a2cc7b919614e36b0f3135df848e01d821cab
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2022-11-06 11:56:49 -0800

    All homework code

--- src/triangle.js
+++ src/triangle.js
@@ -1,3 +1,6 @@
+export default function Triangle(side1, side2, side3) {
+  
+
 function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
@@ -6,4 +9,5 @@ function Triangle(side1, side2, side3) {
 
 Triangle.prototype.checkType = function() {
   return "I can't answer that yet!";
+}
 }
